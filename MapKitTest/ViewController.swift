b683fb64ea9d6dd816e32c6c19676f5c6bb0ba47
ken.yurino <ken.yurino@gmail.com>
2020-08-29 16:49:47 +0900

    Initial Commit

---
+++ MapKitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitTest
+//
+//  Created by 百合野健 on R 2/08/29.
+//  Copyright © Reiwa 2 KenYurino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapKitTestTests/MapKitTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MapKitTestTests.swift
+//  MapKitTestTests
+//
+//  Created by 百合野健 on R 2/08/29.
+//  Copyright © Reiwa 2 KenYurino. All rights reserved.
+//
+
+import XCTest
+@testable import MapKitTest
+
+class MapKitTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
